82dde2994cfd35369e620bb14ddf319d6c04a01d
John (Jack) Brown <jack@brownjohnf.com>
2013-02-16 16:58:53 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoulmateTest::Application.config.secret_token = '62a1bf607f2f0aa6cd03aed4e393390ea46eb7f770411358acc209b677485d2a65f6c2355bd3ac2a6f5e2fe3c146937e2968957ce73685e908dc10714a87596a'
